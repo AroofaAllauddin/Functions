356dbe6ee350590eb0852b3609f267ebfd9e662e
Aroofa Allauddin <160481304+AroofaAllauddin@users.noreply.github.com>
2024-03-29 23:08:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// Example 1 //
+// Function //
+var players = ["Muhammad Amir", "Imad Waseem", "Babar Azam"];
+function playersName() {
+    console.log(players);
+}
+// call fuction //
+playersName();
+// Example 2 //
+var dinner = ["Biryani", "Burger", "Pizza"];
+function dishesName() {
+    console.log(dinner);
+}
+// Call function //
+dishesName();
---
+++ main.ts
@@ -0,0 +1,25 @@
+// Example 1 //
+
+// Function //
+
+let players = ["Muhammad Amir", "Imad Waseem", "Babar Azam"];
+function playersName(){
+
+    console.log(players)
+}
+
+// call fuction //
+
+playersName()
+
+// Example 2 //
+
+let dinner = ["Biryani","Burger","Pizza"];
+function dishesName(){
+
+    console.log(dinner)
+}
+
+// Call function //
+
+dishesName()
